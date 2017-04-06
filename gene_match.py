614f841cbff3f55760b043a4e3dfbb2f12a74d00
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-04-06 14:09:52 +0000

    Update gene_match.py

--- gene_match.py
+++ gene_match.py
@@ -24,3 +24,33 @@ workbook = xlrd.open_workbook(file_location_distinct)
 sheet = workbook.sheet_by_index(0)
 
 hike=4
+
+#distinct gene match 
+
+for index in range(0, 122):
+
+	pathway = sheet.cell_value(index, 0)
+	length = sheet.cell_value(index, 1)
+	length = int(length)
+	essential_count = 0
+
+	for alpha_index in range(0, length):
+
+		gene = sheet.cell_value(index, alpha_index+hike)
+		print(gene)
+
+		for beta_index in range(0, len(lethal_genes)):
+
+			if gene == lethal_genes[beta_index]:
+				essential_count = essential_count+1
+				print('essential_gene', gene)
+			else:
+				essential_count = essential_count
+	print(essential_count)
+	ws.cell(row= index+1, column=4).value = essential_count
+	ws.cell(row=index+1, column=1).value = pathway
+	ws.cell(row=index+1, column=6).value = length
+	ws.cell(row= index+1, column=3).value = str('found')
+	ws.cell(row=index+1, column=5).value = str('lethal out of')
+	wb.save("distinct_gene_to_lethal_1.xlsx")
+"""
