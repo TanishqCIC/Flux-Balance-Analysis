b208d551a90d0f450e709ef2e3b45e4e79835380
Tanishq Kumar Dhangar <tanishqcic@gmail.com>
2017-04-08 16:23:16 +0530

    Update distinct_genes.py

--- distinct_genes.py
+++ distinct_genes.py
@@ -75,3 +75,28 @@ for i in range(0, len(pathway_change)):
 
 with open('distinct_1.txt', 'w') as outfile:
 		json.dump(distinct, outfile)
+"""
+for i in range(0,122) :
+
+	distinct = 0
+
+	pathway = sheet.cell_value(i,0)
+	print(pathway)
+	ws.cell(row=i+1, column=1).value = pathway
+	length = sheet.cell_value(i,1)
+	#print(length)
+	alpha_index = (int(length))
+	#print(alpha_index)
+	j=4
+	flag=0
+	for beta_index in range(3, alpha_index-1):
+		#print(beta_index)
+		cell_gene = sheet.cell_value(i, beta_index)
+		#print(cell_gene)
+		if beta_index == 3:
+			distinct = distinct+1
+			ws.cell(row=i+1, column=j+1).value = cell_gene
+			#print(cell_gene)
+			j=j+1
+			wb.save("pathway_distinct_genes_1.1.xlsx")
+			#print('value saved')
